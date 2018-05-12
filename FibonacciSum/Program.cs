902109fea937e20c458686cbf4e4e601d4f0774e
Scott Cantrell <scottmc0525@gmail.com>
2018-05-12 17:44:11 -0400

    Each new term in the Fibonacci sequence is generated by adding the previous two terms. By starting with 1 and 2, the first 10 terms will be:
    1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
    
    By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.

--- FibonacciSum/Program.cs
+++ FibonacciSum/Program.cs
@@ -1,12 +1,64 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FibonacciSum
 {
-    class Program
+    class EvenFibonacciSum
     {
+        private int currentIndex = 0;
+        List<int> arrayList = new List<int>();
+        private int sum = 0;
+
+        public int CurrentIndex { get => currentIndex; set => currentIndex = value; }
+        public int Sum { get => sum; set => sum = value; }
+
+        public EvenFibonacciSum()
+        {
+            init();
+        }
+
+        public void init()
+        {
+            arrayList.Add(1);
+            arrayList.Add(2);
+            CurrentIndex = 1;
+            Sum = 2;
+        }
+
+        public int GetFibonacciNumbers(int upperLimit)
+        { 
+            while (arrayList[CurrentIndex] < upperLimit)
+            {
+                arrayList.Add((arrayList[CurrentIndex] + arrayList[CurrentIndex - 1]));
+                CurrentIndex++;
+
+                if (arrayList[CurrentIndex] % 2 == 0)
+                {
+                    Sum += arrayList[CurrentIndex];
+                }
+
+                
+            }
+
+            return Sum;
+        }
+
+        public void PrintFibonacci()
+        {
+            foreach(int item in arrayList)
+            {
+                Console.WriteLine("FN: " + item);
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            EvenFibonacciSum euler2 = new EvenFibonacciSum();
+            euler2.GetFibonacciNumbers(4000000);
+            euler2.PrintFibonacci();
+            Console.WriteLine("Sum is " + euler2.Sum);
+            Console.WriteLine("Max index was " + euler2.CurrentIndex);
+            Console.WriteLine("Max Value was " + euler2.arrayList[euler2.CurrentIndex]);
         }
     }
 }
